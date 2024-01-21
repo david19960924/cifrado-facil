be2207f3c33e3b3b3087ee5bdf77d045e6e99ff6
david19960924 <157087459+david19960924@users.noreply.github.com>
2024-01-21 15:08:54 -0300

    Update script.js
    mas opciones de algortimos de cifrado

--- js/script.js
+++ js/script.js
@@ -1,39 +1,86 @@
-// Importa la librería CryptoJS (asegúrate de haber incluido los scripts antes)
-//const CryptoJS = require('crypto-js');
+function readMessage() {
+    /**Obtener datos
+     *              -->cifrado
+     * Mostrar datos
+     */
+    let secretKey = $("#idpassword").val();
+    let message = $("#idmenssage").val();
+    let option = $("#algoritmo").val();
+    let resultado;
 
-// Clave secreta para el cifrado y descifrado (deberías manejar esto de manera segura en un entorno real)
-//let secretKey = 'miClaveSecreta';
+    switch (option) {
 
-// Función para cifrar un mensaje
-function encrypt(message, secretKey) {
-    const ciphertext = CryptoJS.AES.encrypt(message, secretKey);
-    return ciphertext.toString();
+        case "AES":
+
+            resultado = CryptoJS.AES.encrypt(message, secretKey);
+            break;
+        case "DES":
+
+            resultado = CryptoJS.DES.encrypt(message, secretKey);
+            break;
+        case "TripleDES":
+
+            resultado = CryptoJS.TripleDES.encrypt(message, secretKey);
+            break;
+        case "Rabbit":
+
+            resultado = CryptoJS.Rabbit.encrypt(message, secretKey);
+            break;
+        case "RC4":
+
+            resultado = CryptoJS.RC4.encrypt(message, secretKey);
+            break;
+    }
+
+
+    $("#idmenssage").val(resultado.toString());
 }
 
-// Función para descifrar un mensaje
-function decrypt(ciphertext, secretKey) {
-    const bytes = CryptoJS.AES.decrypt(ciphertext, secretKey);
-    const originalText = bytes.toString(CryptoJS.enc.Utf8);
-    return originalText;
-}
-
-
-function readMessage(option) {
+function decryptMessage() {
+    /**Obtener datos
+     *              -->descifrado
+     * Mostrar datos
+     */
+    let option = $("#algoritmo").val();
     let secretKey = $("#idpassword").val();
     let message = $("#idmenssage").val();
     let resultado;
 
     switch (option) {
-        case 1:
-            resultado = encrypt(message, secretKey);
+        case "AES":
+            resultado = CryptoJS.AES.decrypt(message, secretKey);
             break;
-        case 2:
-            resultado = decrypt(message, secretKey);
+        case "DES":
+            resultado = CryptoJS.DES.decrypt(message, secretKey);
+            break;
+        case "TripleDES":
+            resultado = CryptoJS.TripleDES.decrypt(message, secretKey);
+            break;
+        case "Rabbit":
+            resultado = CryptoJS.Rabbit.decrypt(message, secretKey);
+            break;
+        case "RC4":
+            resultado = CryptoJS.RC4.decrypt(message, secretKey);
             break;
     }
-    
-    $("#idmenssage").val(resultado);
-   
+
+    originalText = resultado.toString(CryptoJS.enc.Utf8);
+    $("#idmenssage").val(originalText);
 }
 
 
+
+function copiar() {
+    const textarea = document.getElementById('idmenssage');
+
+    textarea.select();
+  
+    // Intenta copiar el texto al portapapeles
+    document.execCommand('copy');
+
+    textarea.blur();
+
+    $("#copiar").text('\u2714 Copiado!');
+
+    setTimeout(function () { $("#copiar").text('Copiar'); }, 3000);
+}
